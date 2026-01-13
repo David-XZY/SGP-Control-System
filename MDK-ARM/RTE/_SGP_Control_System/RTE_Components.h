83a82a6fe2114b18d5e0c11328ac10a10194eeba
David-XZY <1260372069@qq.com>
2026-01-13 15:08:04 +0800

    Initial commit: Set up project structure and gitignore

---
+++ MDK-ARM/RTE/_SGP_Control_System/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SGP_Control_System' 
+ * Target:  'SGP_Control_System' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
